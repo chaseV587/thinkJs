1441dad2840cd76fa6f6df3cbb1d07fe6c93277a
Chase <943616235@qq.com>
2018-08-28 16:18:04 +0800

    方便停车模版

--- easyPark-master/src/config/adapter.js
+++ easyPark-master/src/config/adapter.js
@@ -36,13 +36,13 @@ exports.model = {
   },
   mysql: {
     handle: mysql,
-    database: '',
-    prefix: 'think_',
+    database: 'easypark',
+    prefix: '',
     encoding: 'utf8',
     host: '127.0.0.1',
-    port: '',
+    port: '3306',
     user: 'root',
-    password: 'root',
+    password: '',
     dateStrings: true
   }
 };
---
+++ easyPark-master/src/controller/index.js
@@ -0,0 +1,21 @@
+const Base = require('./base.js');
+
+module.exports = class extends Base {
+  async indexAction() {
+    console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    const user = this.model('easypark_user'); // controller 里实例化模型
+    const data = await user.select();
+    console.log(data)
+    // let userinfo =await this.session('userinfo')
+    // if (!think.isEmpty(userinfo)){
+    //   this.assign('userinfo',userinfo);
+    // }else{
+    //   return this.redirect('/admin/index');
+    // }
+    this.assign('title',"测试网页之hello world!");
+    // return this.display();
+    return this.success({
+      user: data
+    })
+  }
+};
