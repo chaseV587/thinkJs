c105339976ca4a4f99abd05a7675b79055ffeac2
Chase <943616235@qq.com>
2018-08-31 09:46:29 +0800

    合并

--- easy-park-master/src/admin/controller/base.js
+++ easy-park-master/src/admin/controller/base.js
@@ -1,19 +1,5 @@
 module.exports = class extends think.Controller {
-  // __before() {
-  //
-  // }
-  async __before(){
+  __before() {
     console.log('__before');
-    // // 根据token值获取用户id
-    // this.ctx.state.token = this.ctx.header['x-nideshop-token'] || '';
-    // const tokenSerivce = think.service('token', 'admin');
-    // this.ctx.state.userId = await tokenSerivce.getUserId(this.ctx.state.token);
-    //
-    // // 只允许登录操作
-    // if (this.ctx.controller !== 'auth') {
-    //   if (this.ctx.state.userId <= 0) {
-    //     return this.fail(401, '请先登录');
-    //   }
-    // }
   }
 };
