0f243e4d6e585e067c701909b2b5a5d9981958d5
Chase <943616235@qq.com>
2018-09-13 18:01:53 +0800

    车位管理首页

---
+++ ep-app-master/src/page/manage/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper" append="tree">
+    <ums-header
+      height="90px"
+      fontSize="36px"
+      backgroundColor="#38DCB7"
+      @onLeftPartClick="leftClick"
+      @onRightPartClick="rightClick"
+      class="header"
+      title="车位管理"
+    >
+    <image slot="right" class="header-icon site-icon"  src='../../../static/image/icon-03.png'></image>
+    </ums-header>
+    
+    
+  </div>
+</template>
+
+<style>
+.wrapper { 
+  width: 720px;
+  background-color: #fefefe; 
+  flex-direction: column;
+}
+.header-icon{
+  width: 40px;
+  height: 40px; 
+  margin: 25px 25px;
+}
+  
+</style>
+
+<script>
+import {UmsHeader} from 'ums-comp'
+  export default {
+    components:{
+      UmsHeader
+    },
+    data: function () {
+      return {
+        errInfo: '',
+        username: 'test',
+        password: '123456'
+      }
+    },
+    computed: {
+    },
+    methods:{
+      init() {
+      },
+      
+    },
+    created() {
+      this.init()
+      console.log('login.vue')
+    },
+  }
+</script>
--- ep-app-master/src/router/index.js
+++ ep-app-master/src/router/index.js
@@ -6,11 +6,14 @@ import loginView from '../page/auth/login/login.vue'
 import registerView from '../page/auth/register/register.vue'
 // 首页
 import homeView from '../page/home/index.vue'
+// 车位管理 首页
+import parkListView from '../page/manage/index/index.vue'
 
 export default new VueRouter({
   routes: [
     { path: '/login', component: loginView }, // 登录页面
     { path: '/register', component: registerView }, // 注册页面
     { path: '/home', component: homeView }, // 首页页面
+    { path: '/manage-index', component: parkListView }, // 首页页面
   ]
 })
