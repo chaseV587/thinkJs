d3573234a52384db836f1563b45e90a4a21676e7
Chase <943616235@qq.com>
2018-09-27 17:41:22 +0800

    存储 用户id

--- ep-app-master/src/store/modules/user.js
+++ ep-app-master/src/store/modules/user.js
@@ -1,11 +1,14 @@
 export default {
   state: {
     userInfo:{},
-    userId: '84569552'
+    userId: '3df93c50-c226-11e8-b2e9-83512a4a48ba'
   },
   mutations: {
     setUserInfo (state, param) {
       state.userInfo = param
+    },
+    setUserId (state, param) {
+      state.userId = param
     },
   },
   actions: {
